96c152678030e2c8ea513a625a8e7a661d033386
soumalyasmp <soumalyasmp1845@gmail.com>
2025-01-31 19:52:59 +0530

    git is updated with server links

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration
 const corsOptions = {
-    origin: ["*"
+    origin: ['http://localhost:5173','https://task-and-reminder-calender-system.vercel.app'
     ],
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -37,7 +37,7 @@ app.use('/api/profile', require('./route/userroute'));
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: ["*"], 
+        origin: ['http://localhost:5173','https://task-and-reminder-calender-system.vercel.app'], 
         methods: ['GET', 'POST'],
         credentials: true,
     }
